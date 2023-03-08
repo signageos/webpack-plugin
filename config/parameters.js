c552d53f6d305359464658148585ab56563c300a
Michael Zabka <zabka.michael@gmail.com>
2023-03-08 15:32:22 +0000

    Add clientVersions into user-agent of RestApi and fix types

--- config/parameters.js
+++ config/parameters.js
@@ -1,3 +1,5 @@
+const packageConfig = require('../package.json');
 module.exports = {
+    version: packageConfig.version,
     apiUrl: process.env.API_URL ? process.env.API_URL : "https://api.signageos.io"
 }

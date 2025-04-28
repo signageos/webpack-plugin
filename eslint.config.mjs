a151fdfe8d605182447ad95757b1c38af31a0457
Mikuláš Sychra <mikulas.sychra@gmail.com>
2025-04-28 15:05:56 +0200

    Updated dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,20 +7,20 @@ const compat = new FlatCompat();
 
 /** @type {import("typescript-eslint").Config} */
 const config = tseslint.config(compat.extends('prettier'), compat.config(codeStyle), {
-	files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts', '**/*.mjs', '**/*.js'],
-	languageOptions: {
-		parser: tseslint.parser,
-		parserOptions: {
-			project: ['./tsconfig.json', './tsconfig.test.json'],
-		},
-	},
-	rules: {
-		'@typescript-eslint/no-empty-function': 'off',
-		'unused-imports/no-unused-vars': 'off',
-		'unused-imports/no-unused-imports': 'off',
+    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts', '**/*.mjs', '**/*.js'],
+    languageOptions: {
+        parser: tseslint.parser,
+        parserOptions: {
+            project: ['./tsconfig.json', './tsconfig.test.json'],
+        },
+    },
+    rules: {
+        '@typescript-eslint/no-empty-function': 'off',
+        'unused-imports/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'off',
         //"no-use-before-declare": false,
         //"no-shadowed-variable": false
-	},
+    },
     ignores: [
         'dist/**/* ',
         'build/**/*',
